01ea87b555455f3c7fd6bc226d8865e4882dec8e
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-09-02 09:02:38 -0300

    Added logs vector and improved comments

--- src/player.cpp
+++ src/player.cpp
@@ -2,175 +2,74 @@
  * @file player.cpp
  * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
  * @brief Implementation of the players class with its attributes and methods.
- * @version 0.1
+ * @version 1.0
  * @date 2023-08-30
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include "player.h"
 
-/**
- * @brief Construct a new Player object with the specified name.
- * 
- * @param name_ Player name.
- */
-Player::Player(std::string name_, PlayerType type_) {
-    setName(name_);
-    type = type_;
-    score = DEFAULT_SCORE;
-    victories = DEFAULT_VICTORIES;
-    defeats = DEFAULT_DEFEATS;
-    high_score = DEFAULT_HIGH_SCORE;
+// Constructor with name
+Player::Player(std::string name_) {
+   setName(name_);
+   turn_score = DEFAULT_SCORE;
+   score = DEFAULT_SCORE;
 }
 
-/**
- * @brief Destroy the Player object.
- * 
- */
-Player::~Player() { }
+// Destructor
+Player::~Player() {
+}
 
-/**
- * @brief Get player name.
- * 
- * @return The name.
- */
+// Get player name
 std::string Player::getName() const {
-    return name;
+   return name;
 }
 
-/**
- * @brief Get player type.
- * 
- * @return The type. 
- */
-PlayerType Player::getType() const {
-    return type;
-}
-
-/**
- * @brief Get player turn score.
- * 
- * @return The turn score. 
- */
+// Get player turn score
 int Player::getTurnScore() const {
-    return turn_score;
+   return turn_score;
 }
 
-/**
- * @brief Get player score.
- * 
- * @return The score. 
- */
+// Get player score
 int Player::getScore() const {
-    return score;
+   return score;
 }
 
-/**
- * @brief Get number of wins from the player.
- * 
- * @return Number of victories.
- */
-int Player::getVictories() const {
-    return victories;
+// Get game logs
+std::vector<utils::RoundLog> const *Player::getLogs() const {
+   return &logs;
 }
 
-/**
- * @brief Get the player's number of defeats
- * 
- * @return Number of defeats.
- */
-int Player::getDefeats() const {
-    return defeats;
-}
-
-/**
- * @brief Get the player's high score.
- * 
- * @return The high score.
- */
-int Player::getHighScore() const {
-    return high_score;
-}
-
-/**
- * @brief Set the player name.
- * 
- * @param name_ Name to set.
- */
+// Set player name
 void Player::setName(std::string name_) {
-    utils::leftTrim(name_);
+   utils::leftTrim(name_);
 
-    if (name_.empty()) {
-        name = DEFAULT_NAME;
-    } else {
-        name = name_;
-    }
+   if (name_.empty()) {
+      name = DEFAULT_NAME;
+   } else {
+      name = name_;
+   }
 }
 
-/**
- * @brief Add score to turn.
- * 
- * @param turn_score_ Additional turn score.
- */
+// Add turn score
 void Player::addTurnScore(int turn_score_) {
-    turn_score += turn_score_ > MINIMUM_SCORE ? turn_score_ : MINIMUM_SCORE;
+   turn_score += turn_score_ > MINIMUM_SCORE ? turn_score_ : MINIMUM_SCORE;
 }
 
-/**
- * @brief Add score to total.
- * 
- * @param score_ Additional score.
- */
-void Player::addScore() {
-    score += turn_score;
-    clearTurnScore();
-}
-
-/**
- * @brief Add new victory.
- * 
- */
-void Player::addVictory() {
-    ++victories;
-    checkHighScore();
-}
-
-/**
- * @brief Add new defeat.
- * 
- */
-void Player::addDefeat() {
-    ++defeats;
-    checkHighScore();
-}
-
-/**
- * @brief Clear turn score.
- * 
- */
+// Clear turn score
 void Player::clearTurnScore() {
-    turn_score = DEFAULT_SCORE;
+   turn_score = DEFAULT_SCORE;
 }
 
-/**
- * @brief Clear score.
- * 
- */
-void Player::clearScore() {
-    score = DEFAULT_SCORE;
+// Add score
+void Player::addScore() {
+   score += turn_score;
+   clearTurnScore();
 }
 
-/**
- * @brief Checks if the game's score was the player's highest score.
- * 
- */
-void Player::checkHighScore() {
-    if (score > high_score) {
-        high_score = score;
-    }
-
-    clearTurnScore();
-    clearScore();
+// Add log
+void Player::addLog(utils::RoundLog log_) {
+   logs.push_back(log_);
 }
--- src/player.h
+++ src/player.h
@@ -2,82 +2,110 @@
  * @file player.h
  * @author Pedro Lucas (pedrolucas.jsrn@gmail.com)
  * @brief Definition of the player class with its attributes and methods.
- * @version 0.1
+ * @version 1.0
  * @date 2023-08-30
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #ifndef PLAYER_H
 #define PLAYER_H
 
 #include "utils.h"
+#include <vector>
 
-#define DEFAULT_NAME "Jack" /**< Default name for the player. */
-#define DEFAULT_SCORE 0 /**< Default score for the player. */
-#define DEFAULT_VICTORIES 0 // Default victories for the player. */
-#define DEFAULT_DEFEATS 0 /**< Default defeats for the player. */
-#define DEFAULT_HIGH_SCORE 0 // Player default high score. */
-#define MINIMUM_SCORE 0 /**< Minimum score in play. */
+#define DEFAULT_NAME "Ned"          /**< Default name for the player */
+#define DEFAULT_MACHINE_NAME "Ilyn" /**< Default name for the machine */
+#define DEFAULT_SCORE 0             /**< Default score for the player */
+#define DEFAULT_ROUND 0             /**< Default score round for the player */
+#define MINIMUM_SCORE 0             /**< Minimum score in round */
 
 /**
- * @brief Player type.
- * 
+ * @brief Definition of player class
+ *
  */
-enum PlayerType {
-    Human,
-    Machine,
-};
-
-// Player class.
 class Player {
-public:
-    // Constructor with name and type.
-    Player(std::string name_ = DEFAULT_NAME, PlayerType type_ = Machine);
-    // Destructor.
-    ~Player();
+   /* Definition of attributes */
+   std::string name;                  /**< Player name */
+   int turn_score;                    /**< Player turn score */
+   int score;                         /**< Player score */
+   std::vector<utils::RoundLog> logs; /**< Game logs */
+ public:
+   /**
+    * @brief Construct a new Player object with the specified name
+    *
+    * @param name_ Player name
+    */
+   Player(std::string name_ = DEFAULT_NAME);
 
-    // Get player name.
-    std::string getName() const;
-    // Get player type.
-    PlayerType getType() const;
-    // Get player turn score.
-    int getTurnScore() const;
-    // Get player score.
-    int getScore() const;
-    // Get number of wins from the player.
-    int getVictories() const;
-    // Get the player's number of defeats
-    int getDefeats() const;
-    // Get the player's high score.
-    int getHighScore() const;
+   /**
+    * @brief Destroy the Player object
+    *
+    */
+   ~Player();
 
-    // Set player name.
-    void setName(std::string name_);
-    // Add turn score.
-    void addTurnScore(int turn_score_);
-    // Add score.
-    void addScore();
-    // Add victory.
-    void addVictory();
-    ///Add defeat.
-    void addDefeat();
-private:
-    std::string name; /**< Player name */
-    PlayerType type; /**< Player type */
-    int turn_score; /**< Player turn score */
-    int score; /**< Player score */
-    int victories; /**< Number of wins */
-    int defeats; /**< Number of defeats */
-    int high_score; /**< Player high score */
+   /**
+    * @brief Get player name
+    *
+    * @return The name
+    */
+   std::string getName() const;
 
-    // Clear turn score.
-    void clearTurnScore();
-    // Clear score.
-    void clearScore();
-    // Check high score.
-    void checkHighScore();
+   /**
+    * @brief Get player turn score
+    *
+    * @return The turn score
+    */
+   int getTurnScore() const;
+
+   /**
+    * @brief Get player score
+    *
+    * @return The score
+    */
+   int getScore() const;
+
+   /**
+    * @brief Get game logs
+    *
+    * @return The logs
+    */
+   std::vector<utils::RoundLog> const *getLogs() const;
+
+   /**
+    * @brief Set the player name
+    *
+    * @param name_ Name to set
+    */
+   void setName(std::string name_);
+
+   /**
+    * @brief Add score to turn
+    *
+    * @param turn_score_ Additional turn score
+    */
+   void addTurnScore(int turn_score_);
+
+   /**
+    * @brief Clear turn score
+    *
+    */
+   void clearTurnScore();
+
+   /**
+    * @brief Add score to total
+    *
+    * @param score_ Additional score
+    */
+   void addScore();
+
+   /**
+    * @brief Adds a new log to the vector
+    *
+    * @param log_ Log to add
+    */
+   void addLog(utils::RoundLog log_);
 };
 
 #endif // PLAYER_H
