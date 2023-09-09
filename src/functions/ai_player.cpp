194d176e399a12bb2e0de2aa770a3734aa81c0e7
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2023-09-09 10:05:23 -0300

    Updated artificial intelligence to compare multiple opponents

--- src/functions/ai_player.cpp
+++ src/functions/ai_player.cpp
@@ -12,22 +12,42 @@
 #include "ai_player.hpp"
 
 // Decides whether the player should play
-bool decideRoll(Player const *player, Player const *adversary) {
-   int difference { adversary->getScore() - player->getScore() };
-   /* The number 8 is defined by the algorithm presented in the reference */
-   int max_value { DEFAULT_SCORE_FETCHED + difference / 8 };
-   int total_score { player->getScore() + player->getTurnScore() };
-   int missing_score { WINNERS_SCORE - player->getScore() };
+bool decideRoll(Player const *player, Player* const players, 
+   size_t number_of_players) 
+{
+   Player adversaries[--number_of_players];
+   getAdversaries(player, players, number_of_players, adversaries);
+
+   bool extreme_danger;
+   bool danger;
+   Player* best_player { adversaries };
+
+   for (size_t index { 0 }; index < number_of_players; ++index) {
+      if (adversaries[index].getScore() >= EXTREME_DANGER_ZONE) {
+         extreme_danger = true;
+      } else if (adversaries[index].getScore() >= DANGER_ZONE) {
+         danger = true;
+      }
+
+      if (adversaries[index].getScore() > best_player->getScore()) {
+         best_player = adversaries + index;
+      }
+   }
 
    bool roll { false };
+   int total_score { player->getScore() + player->getTurnScore() };
+   int difference { best_player->getScore() - player->getScore() };
+   /* The number 8 is defined by the algorithm presented in the reference */
+   int max_value { DEFAULT_SCORE_FETCHED + difference / 8 };
+   int missing_score { WINNERS_SCORE - player->getScore() };
 
    if (total_score >= WINNERS_SCORE) {
       roll = false;
-   } else if (adversary->getScore() >= EXTREME_DANGER_ZONE) {
+   } else if (extreme_danger) {
       roll = true;
    } else if (player->getTurnScore() < max_value) {
       roll = true;
-   } else if (adversary->getScore() >= DANGER_ZONE) {
+   } else if (danger) {
       /*
        * The following logic, with the whole number divided by 3/5, considers
        * avoiding very long moves even if the opponent is in the danger zone.
@@ -40,4 +60,16 @@ bool decideRoll(Player const *player, Player const *adversary) {
    }
 
    return roll;
+}
+
+// Filters the current player from other opponents
+void getAdversaries(Player const *player, Player* const players, size_t number_of_players, Player* adversaries) {
+   size_t position { 0 };
+
+   for (size_t index { 0 }; index < number_of_players; ++index) {
+      if (player != &players[index]) {
+         adversaries[position] = players[index];
+         ++position;
+      }
+   }
 }
